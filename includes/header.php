d76de4e5d9fea50bcde2604d750b439411947126
Goat3ggs <stefpandi12@gmail.com>
2024-09-11 18:14:16 +0300

    create serach form component and include it in the header.php

--- includes/header.php
+++ includes/header.php
@@ -64,27 +64,11 @@
                             </a>
                         </li>
                     </ul>
-                <?php } ?>
-                <form class="d-flex" role="search">
-                    <input
-                        class="form-control me-2"
-                        type="search"
-                        placeholder="Search"
-                        aria-label="Search">
-                    <button
-                        class="btn btn-outline-success"
-                        type="submit">
-                        Search
-                    </button>
-                </form>
+                <?php }
+            include("search-form.php") ?>
                 </div>
         </div>
     </nav>
-
-
-
-
-
     <div class="container">
 
         <?php
