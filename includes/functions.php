287a8d645c36361f5cd4964b6f2ff8a2c471ef11
Goat3ggs <stefpandi12@gmail.com>
2024-09-12 17:29:50 +0300

    create function that display if the movie is older than 40

--- includes/functions.php
+++ includes/functions.php
@@ -1,9 +1,21 @@
 <?php
 
+// mktime(hours, minutes, seconds); 
+// you can use all three parameters or, in this case, only two.
+
 function runtime_prettier($min)
 {
     return date('G \h\o\u\r i', mktime(0, $min)) . " minutes";
 }
 
-// mktime(hours, minutes, seconds); 
-// you can use all three parameters or, in this case, only two.
+function check_old_movie($year)
+{
+    $yearDiff = date('Y') - $year;
+
+    if ($yearDiff <= 40) {
+        return "FALSE";
+    } else {
+        echo '<span class="badge rounded-pill  text-bg-warning">Old movie: ' . $yearDiff . ' years</span>';
+        return $yearDiff;
+    }
+}
--- movie-3.php
+++ movie-3.php
@@ -6,7 +6,7 @@
     <img src="https://m.media-amazon.com/images/M/MV5BMGU2NzRmZjUtOGUxYS00ZjdjLWEwZWItY2NlM2JhNjkxNTFmXkEyXkFqcGdeQXVyNjU0OTQ0OTY@._V1_.jpg" alt="Terminator 2 cover image" class="card-img-top">
   </div>
   <div class="col-9">
-    <h2>1991</h2>
+    <h2>1991 <?php check_old_movie(1981) ?></h2>
     <p>In this sequel set eleven years after "The Terminator," young John Connor (Edward Furlong), the key to civilization's victory over a future robot uprising, is the target of the shape-shifting T-1000 (Robert Patrick), a Terminator sent from the future to kill him. Another Terminator, the revamped T-800 (Arnold Schwarzenegger), has been sent back to protect the boy. As John and his mother (Linda Hamilton) go on the run with the T-800, the boy forms an unexpected bond with the robot.</p>
     <p>Directed By: <span class="movie-info-bold">James Cameron</span></p>
     <p>Runtime: <span class="movie-info-bold"><?php echo runtime_prettier(136) ?></span></p>
