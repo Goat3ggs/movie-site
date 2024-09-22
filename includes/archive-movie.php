29358e5b4cad69eba659f01b59aca589b1d2a7a0
Goat3ggs <stefpandi12@gmail.com>
2024-09-22 14:25:41 +0300

    display onlu 100 char for plot

--- includes/archive-movie.php
+++ includes/archive-movie.php
@@ -2,7 +2,16 @@
     <img src="<?php echo $movie["posterUrl"] ?>" class="card-img-top" alt="titanic image">
     <div class="card-body">
         <h5 class="card-title"><?php echo $movie["title"] ?></h5>
-        <p class="card-text"><?php echo $movie["plot"] . "..."  ?></p>
+        <p class="card-text">
+            <?php
+            $plot = $movie["plot"];
+            if (strlen($plot) > 100) {
+                echo substr($movie["plot"], 0, 100) . "...";
+            } else {
+                echo $plot;
+            }
+            ?>
+        </p>
         <a href="movie.php?movie_id=<?php echo $movie["id"] ?>" class="btn btn-primary">Read more</a>
     </div>
 </div>
