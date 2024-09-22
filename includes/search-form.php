ea98fe867c8b627101f6a2cf9030cecf6591e290
Goat3ggs <stefpandi12@gmail.com>
2024-09-22 14:24:40 +0300

    make possible for user to input value and store the information then do smth

--- includes/search-form.php
+++ includes/search-form.php
@@ -1,9 +1,11 @@
-<form class="d-flex" role="search">
+<form class="d-flex" role="search" method="get" action="search-results.php">
     <input
         class="form-control me-2"
         type="search"
         placeholder="Search"
-        aria-label="Search">
+        aria-label="Search"
+        name="search"
+        value="<?php echo isset($_GET['search']) ? htmlspecialchars($_GET['search']) : '' ?>">
     <button
         class="btn btn-outline-success"
         type="submit">
